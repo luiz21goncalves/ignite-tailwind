9a4016428958112777674617a454a623a2624a14
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-03 07:41:36 -0300

    feat: add used space widge component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,10 +2,11 @@ import { Search } from 'lucide-react'
 
 import { Logo } from './Logo'
 import { MainNavigation } from './MainNavigation'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -18,6 +19,7 @@ export function Sidebar() {
       </div>
 
       <MainNavigation />
+      <UsedSpaceWidget />
     </aside>
   )
 }
