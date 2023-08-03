06049cf77c9c8d2e7fe06e22a70555c87cd88977
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-02 21:06:29 -0300

    feat: add sidebar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
     },
   },
   plugins: [],
