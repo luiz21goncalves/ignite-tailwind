6146dcf9cdf54f08f39e578a66c5d5cb4f41b6fb
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-04 20:40:39 -0300

    feat: add form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en" className="antialiased">
       <body className={inter.className}>
-        <div className="grid min-h-screen w-screen grid-cols-app">
+        <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
