30e7b57a69047aa391a6392359c1142b1dbd7672
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-19 09:36:49 -0300

    feat: add styled focus to the input component

--- src/components/Form/Input/InputRoot.tsx
+++ src/components/Form/Input/InputRoot.tsx
@@ -9,7 +9,7 @@ export function InputRoot(props: InputRootProps) {
 
   return (
     <div
-      className="flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm transition-colors focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...attrs}
     >
       {children}
