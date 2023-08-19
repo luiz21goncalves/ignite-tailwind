f2a034aea4dce3a745bccf2a1b3ebfc1b2b15a53
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-19 19:13:23 -0300

    feat: make a sidebar collapsible on mobile devices

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,9 @@
-import { Search } from 'lucide-react'
+'use client'
 
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Menu, Search } from 'lucide-react'
+
+import { Button } from '../Button'
 import { Input } from '../Form/Input'
 import { Logo } from './Logo'
 import { MainNavigation } from './MainNavigation'
@@ -8,20 +12,32 @@ import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="fixed inset-0 z-20 flex h-screen flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
-      <Logo />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="h-6 w-6" />
+          </Button>
+        </Collapsible.Trigger>
+      </div>
 
-      <Input.Root>
-        <Input.Icon>
-          <Search className="text-zinc-500" />
-        </Input.Icon>
-        <Input.Control placeholder="Search" />
-      </Input.Root>
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <Input.Root>
+          <Input.Icon>
+            <Search className="text-zinc-500" />
+          </Input.Icon>
+          <Input.Control placeholder="Search" />
+        </Input.Root>
 
-      <MainNavigation />
-      <UsedSpaceWidget />
-      <div className="h-px bg-zinc-200" />
-      <Profile />
-    </aside>
+        <MainNavigation />
+        <UsedSpaceWidget />
+        <div className="h-px bg-zinc-200" />
+        <Profile />
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
