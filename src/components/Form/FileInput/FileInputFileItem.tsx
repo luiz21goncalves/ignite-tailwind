49a1b8984ba155b5054f819e71969b0b91d143a0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-21 19:04:00 -0300

    feat: add a dark theme in the file input component

--- src/components/Form/FileInput/FileInputFileItem.tsx
+++ src/components/Form/FileInput/FileInputFileItem.tsx
@@ -8,18 +8,20 @@ const fileItem = tv({
   slots: {
     container:
       'group flex items-start gap-4 rounded-lg border border-zinc-200 p-4',
-    icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600',
+    icon: 'rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-500',
     deleteButton: 'transition-colors',
   },
 
   variants: {
     state: {
-      progress: { container: '' },
-      complete: { container: 'border-violet-500' },
+      progress: { container: 'dark:border-zinc-700' },
+      complete: { container: 'border-violet-500 dark:border-violet-300/30' },
       error: {
-        container: 'bg-error-25 border-error-300',
-        icon: 'border-error-50 bg-error-100 text-error-600',
-        deleteButton: 'text-error-700 hover:text-error-900',
+        container:
+          'bg-error-25 border-error-300 dark:bg-error-500/5 dark:border-error-500/30',
+        icon: 'border-error-50 bg-error-100 text-error-600 dark:bg-error-500/5 dark:border-error-500/30 dark:text-error-400',
+        deleteButton:
+          'text-error-700 hover:text-error-900 dark:text-error-400 dark:hover:text-error-300',
       },
     },
   },
@@ -52,15 +54,17 @@ export function FileInputFileItem(props: FileInputFileItemProps) {
       {hasError ? (
         <div className="flex flex-1 flex-col items-start">
           <div className="flex flex-col">
-            <span className="text-sm font-medium text-error-700">
+            <span className="text-sm font-medium text-error-700 dark:text-error-400">
               Upload failed, please tray again.
             </span>
-            <span className="text-sm text-error-600">{name}</span>
+            <span className="text-sm text-error-600 dark:text-error-500">
+              {name}
+            </span>
           </div>
 
           <button
             type="button"
-            className="text-sm font-semibold text-error-700 transition-colors hover:text-error-900"
+            className="text-sm font-semibold text-error-700 transition-colors hover:text-error-900 dark:text-error-400 dark:hover:text-error-300"
           >
             Try again
           </button>
@@ -68,19 +72,21 @@ export function FileInputFileItem(props: FileInputFileItemProps) {
       ) : (
         <div className="flex flex-1 flex-col items-start">
           <div className="flex flex-col">
-            <span className="text-sm font-medium text-zinc-700">{name}</span>
-            <span className="text-sm text-zinc-500">{formattedSize}</span>
+            <span className="text-sm font-medium text-zinc-700 dark:text-zinc-100">
+              {name}
+            </span>
+            <span className="text-sm  dark:text-zinc-400">{formattedSize}</span>
           </div>
 
           <div className="flex w-full items-center gap-3">
-            <div className="h-2 flex-1 overflow-hidden rounded-full bg-zinc-100">
+            <div className="h-2 flex-1 overflow-hidden rounded-full bg-zinc-100 dark:bg-zinc-600">
               <div
-                className="h-full bg-violet-600"
+                className="h-full bg-violet-600 dark:bg-violet-400"
                 style={{ width: isCompleted ? '100%' : '80%' }}
               />
             </div>
 
-            <span className="text-sm font-medium text-zinc-900">
+            <span className="text-sm font-medium text-zinc-900 dark:text-zinc-300">
               {isCompleted ? '100%' : '80%'}
             </span>
           </div>
--- src/components/Form/FileInput/FileInputFileList.tsx
+++ src/components/Form/FileInput/FileInputFileList.tsx
@@ -19,7 +19,7 @@ export function FileInputFileList() {
             key={key}
             name={file.name}
             size={file.size}
-            state="error"
+            state="complete"
           />
         )
       })}
