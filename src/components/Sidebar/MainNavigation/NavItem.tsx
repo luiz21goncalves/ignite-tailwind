f11baecaedebf328c5e398fa85d36700dd6a27f8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-21 08:27:21 -0300

    feat: add a dark theme in the sidebar component

--- src/components/Sidebar/MainNavigation/NavItem.tsx
+++ src/components/Sidebar/MainNavigation/NavItem.tsx
@@ -13,13 +13,13 @@ export function NavItem(props: NavItemProps) {
   return (
     <a
       href=""
-      className="group flex items-center gap-2 rounded px-3 py-2 transition-colors hover:bg-violet-50"
+      className="group flex items-center gap-2 rounded px-3 py-2 transition-colors hover:bg-violet-50 dark:hover:bg-zinc-800"
     >
       <Icon className="h-5 w-5 text-zinc-500" />
-      <span className="font-medium text-zinc-700 transition-colors group-hover:text-violet-500">
+      <span className="font-medium text-zinc-700 transition-colors group-hover:text-violet-500 dark:text-zinc-300 dark:group-hover:text-violet-300">
         {title}
       </span>
-      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition-colors group-hover:text-violet-300" />
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition-colors group-hover:text-violet-300 dark:text-zinc-600" />
     </a>
   )
 }
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -12,10 +12,10 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Luiz Gonçalves
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           luizhbgoncalves@gmail.com
         </span>
       </div>
