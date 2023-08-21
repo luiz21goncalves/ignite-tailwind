037ec111dd4172b0046980c24f8879f5ee1bfb2b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-21 19:03:31 -0300

    feat: add a dark theme in the tab component

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -14,7 +14,7 @@ export function TabItem(props: TabItemProps) {
 
   return (
     <Tabs.Trigger
-      className="group relative px-1 pb-4 text-sm font-medium text-zinc-400 outline-none transition-colors hover:text-violet-700 data-[state=active]:text-violet-700"
+      className="group relative px-1 pb-4 text-sm font-medium text-zinc-400 outline-none transition-colors hover:text-violet-700 data-[state=active]:text-violet-700 dark:text-zinc-400 dark:hover:text-violet-300 dark:data-[state=active]:text-violet-300"
       value={value}
     >
       <span className="whitespace-nowrap rounded transition-colors group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
@@ -23,7 +23,7 @@ export function TabItem(props: TabItemProps) {
       {isSelected && (
         <motion.div
           layoutId="active-tab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 dark:bg-violet-300"
         />
       )}
     </Tabs.Trigger>
