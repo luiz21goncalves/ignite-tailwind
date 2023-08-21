ad44bcaff7426e78b78859075a94afb7a5f001f6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-21 08:26:35 -0300

    feat: add a dark theme in the input component

--- src/components/Form/Input/InputControl.tsx
+++ src/components/Form/Input/InputControl.tsx
@@ -7,7 +7,7 @@ export function InputControl(props: InputControlProps) {
 
   return (
     <input
-      className="w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:text-zinc-100 dark:placeholder-zinc-400"
       {...attrs}
     />
   )
--- src/components/Form/Input/InputRoot.tsx
+++ src/components/Form/Input/InputRoot.tsx
@@ -1,5 +1,7 @@
 import { ComponentProps, ReactNode } from 'react'
 
+import { twMerge } from 'tailwind-merge'
+
 type InputRootProps = ComponentProps<'div'> & {
   children: ReactNode
 }
@@ -9,7 +11,11 @@ export function InputRoot(props: InputRootProps) {
 
   return (
     <div
-      className="flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm transition-colors focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
+      className={twMerge(
+        'flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm transition-colors',
+        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/10',
+      )}
       {...attrs}
     >
       {children}
